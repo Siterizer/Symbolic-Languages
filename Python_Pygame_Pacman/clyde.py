6da7a6dc8cb13ef7cfed3e7c831938e69e90af2e
Siterizer <adamwilhelmmacura@gmail.com>
2019-04-26 14:51:21 +0200

    Clyde is working now

--- Python_Pygame_Pacman/clyde.py
+++ Python_Pygame_Pacman/clyde.py
@@ -13,20 +13,31 @@ class Clyde(character.Character):
         self.animation_mode = 0
         self.inactivity = 0
         self.actual_vertex_number = var.get_vertex_number(x, y)
-        self.last_vertices = ()
+        self.last_vertices = (self.actual_vertex_number, self.actual_vertex_number)
         self.route = []
         self.helper = 0
 
     def move(self):
-        rd = random.randint(0, 3)
-        if rd == 0:
-            self.move_right()
-        elif rd == 1:
-            self.move_left()
-        elif rd == 2:
+
+        if var.check_if_new_vertex_position(self.x, self.y):
+            self.last_vertices = (var.get_vertex_number(self.x, self.y), self.last_vertices[1])
+            self.route = var.graph.dijkstra(self.last_vertices[0], self.last_vertices[1])
+        if len(self.route) == 0:
+            rd = random.randint(1, 67)
+            self.route = var.graph.dijkstra(self.actual_vertex_number, rd)
+            self.last_vertices = (self.actual_vertex_number, rd)
+        print(self.route)
+        a, b = var.vertex[self.route[0]], var.vertex[self.route[1]]
+        x = a[0] - b[0]
+        y = a[1] - b[1]
+        if y > 0:
             self.move_up()
-        elif rd == 3:
+        if y < 0:
             self.move_down()
+        if x > 0:
+            self.move_left()
+        if x < 0:
+            self.move_right()
 
 
 
